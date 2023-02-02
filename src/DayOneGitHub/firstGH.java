34d1536c38dad1c43c06b2400bebe1eb4d440519
aselshaibekova <7aselshaibekova@gmail.com>
2023-02-02 22:10:42 +0600

    I added some math details

---
+++ src/DayOneGitHub/firstGH.java
@@ -0,0 +1,8 @@
+package DayOneGitHub;
+
+public class firstGH {
+    public static void main(String[] args) {
+        System.out.println("Hey GitHub");
+        System.out.println("1+1");
+    }
+}
